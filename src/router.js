ebd8a76631eeb6e4d5dfc8d9b07cfc35aaa615cf
opusvega <parag.kulkarni@opusconsulting.com>
2018-02-01 16:06:27 +0530

    PM2 enabled Update

--- src/router.js
+++ src/router.js
@@ -8,16 +8,10 @@ const chalk = require('chalk');
 
 
 let appRouter = function(app) {
-    // Configuration and Property Lookup type requests
-    // app.get("/PaymentType", function(req, res) {
-    //     //res.send("Hello World");
-    //     let PaymentType = config.PaymentType;
-    // 	res.send(PaymentType);
-    // });
-
-
+ 
     app.get('/',function(req,res){
         console.log(chalk.green('Inside=====GEt'));
+        //console.log(chalk.green(req.ip));
         res.json('Hello World');
     });
     app.get('/getCandidate/:eid',async function(req,res){
@@ -25,16 +19,6 @@ let appRouter = function(app) {
         await apiTestFunctionController.getEmail(req,res);
         console.log(chalk.green('Exiting GET Candidate'));
     });
-    // app.get('/getCandidate',async function(req,res){
-    //     console.log('Entering GET Candidate');
-    //     await apiTestFunctionController.getEmail(req,res);
-    //     console.log('Exiting GET Candidate');
-    // });
-    // app.get('/verifymail/:mail',function(req,res){
-    //     console.log('Entering GET Verify Mail');
-    //     await apiTestFunctionController.getVerifyMail(req,res);
-    //     console.log('Exiting GET Verify Mail');
-    // });
     app.get('/getSkills',async function(req,res){
         //receives skill set for candidate and generates random question bag accordingly
         console.log(chalk.green('Entering GET SKills'));
@@ -46,56 +30,7 @@ let appRouter = function(app) {
         console.log(chalk.green('Entering GET getResult'));
         await apiTestFunctionController.getResult(req,res);
         console.log(chalk.green('Exiting GET getResult'));
-    });
-
-
-    app.post('/hook', async function(req, res) {
-        console.log("inside router app.post: ");
-        console.log(req.body);
-
-        
-        if (req.body.result.metadata.intentName == "welcome-intent") {   
-            console.log("welcome-intent");
-            await apiWelcomeUserFunctionController.apiDefaultWelcome(req, res);
-            console.log("Exiting welcome-intent---->");
-        }
-        
-        if (req.body.result.metadata.intentName == "get-email") {   
-            console.log("get-email");
-            await apiWelcomeUserFunctionController.apiGetEmail(req, res);
-            console.log("Exiting get-email---->");
-        }
-        
-        if (req.body.result.metadata.intentName == "get-email-otp") {   
-            console.log("get-email-otp");
-            await apiWelcomeUserFunctionController.apiGetEmailOtp(req, res);
-            console.log("Exiting get-email-otp---->");
-        }
-        if (req.body.result.metadata.intentName == "get-new-user-name") {   
-            console.log("get-new-user-name");
-            await apiWelcomeUserFunctionController.apiGetUserName(req, res);
-            console.log("get-new-user-name---->");
-        }
-        if (req.body.result.metadata.intentName == "get-new-user-phone") {   
-            console.log("get-new-user-phone");
-            await apiWelcomeUserFunctionController.apiGetUserPhone(req, res);
-            console.log("get-new-user-phone---->");
-        }
-        if (req.body.result.metadata.intentName == "get-new-user-dob") {   
-            console.log("get-new-user-dob");
-            await apiWelcomeUserFunctionController.apiGetUserDob(req, res);
-            console.log("get-new-user-dob---->");
-        }
-        if (req.body.result.metadata.intentName == "test-init") {   
-            console.log("test-init");
-            await apiTestFunctionController.apiTestInit(req, res);
-            console.log("test-init---->");
-        }
-        
-        
-
-    });
-
+    });  
 }
 
 module.exports = appRouter;
--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 let express = require('express');
+let ipfilter = require('express-ipfilter').IpFilter;
 let app = express();
 let bodyParser = require("body-parser");
 let https = require('https');
@@ -10,14 +11,17 @@ let helmet = require('helmet')
 app.use(helmet())
 
 app.use(morgan('combined'))
-
+var ips = ['101.53.164.8'];
+ 
+// Create the server 
+app.use(ipfilter(ips, {mode: 'allow'}));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
 let privateKey  = fs.readFileSync('/etc/ssl/vegabot-certs/server.key', 'utf8');
-let certificate = fs.readFileSync('/etc/ssl/vegabot-certs/vegabot_opusconsulting_com.crt', 'utf8');
+let certificate = fs.readFileSync('/etc/ssl/vegabot-certs/vega.crt', 'utf8');
 let caCertificate = fs.readFileSync('/etc/ssl/vegabot-certs/Geotrust_Intermediate_CA.crt','utf8');
 let credentials = {key: privateKey, cert: certificate, ca: caCertificate}; 
 
@@ -27,5 +31,5 @@ let router = require('./router.js')(app);
 let httpServer = http.createServer(app);
 let httpsServer = https.createServer(credentials, app);
 
-httpServer.listen(8082);
-httpsServer.listen(443);
+//httpServer.listen(8443);
+httpsServer.listen(8444);
